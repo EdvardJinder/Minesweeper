00e8588a3cf9fd90f8d5af8a057e25e9f4e29f75
Edvard Jinder <edvard@jinder.se>
2018-11-30 06:45:38 +0100

    1.0.2

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-let sco = 0;
+
 
 let grey = createColor(202, 202, 202, 79);
 let diff = 24;
@@ -7,347 +7,91 @@ let g = new Grid(diff, diff, 450, 150, 500, 500, "grey", 2, 2);
 g.fill("grey");
 
 for (let i = 0; i < diff; i++) {
-  for (let j = 0; j < diff; j++) {
-    g.cell(i, j).fill(grey);
-    g.cell(i, j).tag = 0;
-  }
+    for (let j = 0; j < diff; j++) {
+        g.cell(i, j).fill(grey);
+        g.cell(i, j).tag = 0;
+    }
 }
 
 for (let i = 0; i < 99; i++) {
-  let r;
-  let c;
-  while (true) {
-    r = randomInt(0, diff - 1);
-    c = randomInt(0, diff - 1);
-    if (g.cell(r, c).imagePath == null) break;
-  }
-  g.cell(r, c).tag = 100;
+    let r;
+    let c;
+    while (true) {
+        r = randomInt(0, diff - 1);
+        c = randomInt(0, diff - 1);
+        if (g.cell(r, c).imagePath == null) break;
+    }
+    g.cell(r, c).tag = 100;
 }
 
 for (let r = 0; r < diff; r++) {
-  for (let c = 0; c < diff; c++) {
-    if (g.cell(r, c).tag >= 100) {
-      if (r < diff - 1) {
-        g.cell(r + 1, c).tag++;
-      }
-      if (r > 0) {
-        g.cell(r - 1, c).tag++;
-      }
-      if (c < diff - 1) {
-        g.cell(r, c + 1).tag++;
-      }
-      if (c > 0) {
-        g.cell(r, c - 1).tag++;
-      }
-      if (r < diff - 1 && c < diff - 1) {
-        g.cell(r + 1, c + 1).tag++;
-      }
-      if (r < diff - 1 && c > 0) {
-        g.cell(r + 1, c - 1).tag++;
-      }
-      if (r > 0 && c < diff - 1) {
-        g.cell(r - 1, c + 1).tag++;
-      }
-      if (r > 0 && c > 0) {
-        g.cell(r - 1, c - 1).tag++;
-      }
+    for (let c = 0; c < diff; c++) {
+        if (g.cell(r, c).tag >= 100) {
+            if (r < diff - 1) {
+                g.cell(r + 1, c).tag++;
+            }
+            if (r > 0) {
+                g.cell(r - 1, c).tag++;
+            }
+            if (c < diff - 1) {
+                g.cell(r, c + 1).tag++;
+            }
+            if (c > 0) {
+                g.cell(r, c - 1).tag++;
+            }
+            if (r < diff - 1 && c < diff - 1) {
+                g.cell(r + 1, c + 1).tag++;
+            }
+            if (r < diff - 1 && c > 0) {
+                g.cell(r + 1, c - 1).tag++;
+            }
+            if (r > 0 && c < diff - 1) {
+                g.cell(r - 1, c + 1).tag++;
+            }
+            if (r > 0 && c > 0) {
+                g.cell(r - 1, c - 1).tag++;
+            }
+        }
     }
-  }
 }
+
+
 
 function empty() {
-  let r = g.selectedCell.row;
-  let c = g.selectedCell.col;
+    let rold = g.selectedCell.row;
+    let cold = g.selectedCell.col;
+    // for (let i = 0; i < 8; i++) {
+    for (let r = (rold - 1); r <= (rold + 1); r++) {
+        for (let c = (cold - 1); c <= (cold + 1); c++) {
+            if (g.cell(r, c).tag < 100) {
+                g.cell(r, c).imagePath = g.cell(r, c).tag + 'p.png'
+                if (g.cell(r, c).tag == 0) {
 
-  if (g.cell(r - 1, c).tag < 100) {
-    g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  }
-  if (g.cell(r - 1, c - 1).tag < 100) {
-    g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  }
-  if (g.cell(r - 1, c + 1).tag < 100) {
-    g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  }
-  if (g.cell(r + 1, c).tag < 100) {
-    g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  }
-  if (g.cell(r + 1, c - 1).tag < 100) {
-    g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  }
-  if (g.cell(r + 1, c + 1).tag < 100) {
-    g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  }
-  if (g.cell(r, c - 1).tag < 100) {
-    g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  }
-  if (g.cell(r, c + 1).tag < 100) {
-    g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  }
 
-  //   if (g.cell(r - 1, c).tag == 0) {
-  //     let r = (g.selectedCell.row -= 1);
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     if (g.selectedCell.tag == 0) {
-  //       return empty();
-  //     }
-  //   }
-
-  //   if (g.cell(r - 1, c - 1).tag == 0) {
-  //     let r = (g.selectedCell.row -= 1);
-  //     let c = (g.selectedCell.col -= 1);
-
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     return empty();
-  //   }
-
-  //   if (g.cell(r - 1, c + 1).tag == 0) {
-  //     let r = (g.selectedCell.row -= 1);
-  //     let c = (g.selectedCell.col += 1);
-
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     if (g.selectedCell.tag == 0) {
-  //       return empty();
-  //     }
-  //   }
-
-  //   if (g.cell(r + 1, c + 1).tag == 0) {
-  //     let r = (g.selectedCell.row += 1);
-  //     let c = (g.selectedCell.col += 1);
-
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     if (g.selectedCell.tag == 0) {
-  //       return empty();
-  //     }
-  //   }
-
-  //   if (g.cell(r + 1, c - 1).tag == 0) {
-  //     let r = (g.selectedCell.row += 1);
-  //     let c = (g.selectedCell.col -= 1);
-
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     if (g.selectedCell.tag == 0) {
-  //       return empty();
-  //     }
-  //   }
-  //   if (g.cell(r + 1, c).tag == 0) {
-  //     let r = (g.selectedCell.row += 1);
-
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     if (g.selectedCell.tag == 0) {
-  //       return empty();
-  //     }
-  //   }
-  //   if (g.cell(r, c - 1).tag == 0) {
-  //     let c = (g.selectedCell.col -= 1);
-
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     if (g.selectedCell.tag == 0) {
-  //       return empty();
-  //     }
-  //   }
-  //   if (g.cell(r, c + 1).tag == 0) {
-  //     let c = (g.selectedCell.col += 1);
-
-  //     if (g.cell(r - 1, c).tag < 100) {
-  //       g.cell(r - 1, c).imagePath = g.cell(r - 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c - 1).tag < 100) {
-  //       g.cell(r - 1, c - 1).imagePath = g.cell(r - 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r - 1, c + 1).tag < 100) {
-  //       g.cell(r - 1, c + 1).imagePath = g.cell(r - 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c).tag < 100) {
-  //       g.cell(r + 1, c).imagePath = g.cell(r + 1, c).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c - 1).tag < 100) {
-  //       g.cell(r + 1, c - 1).imagePath = g.cell(r + 1, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r + 1, c + 1).tag < 100) {
-  //       g.cell(r + 1, c + 1).imagePath = g.cell(r + 1, c + 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c - 1).tag < 100) {
-  //       g.cell(r, c - 1).imagePath = g.cell(r, c - 1).tag + "p.png";
-  //     }
-  //     if (g.cell(r, c + 1).tag < 100) {
-  //       g.cell(r, c + 1).imagePath = g.cell(r, c + 1).tag + "p.png";
-  //     }
-  //     if (g.selectedCell.tag == 0) {
-  //       return empty();
-  //     }
-  //   }
+                }
+            }
+        }
+    }
+    // }
 }
 
+
+
+
+
+
+
+
+
 function update() {
-  if (g.selected) {
-    if (g.selectedCell.tag < 100) {
-      g.selectedCell.imagePath = g.selectedCell.tag + "p.png";
-      if (g.selectedCell.tag == 0) {
-        return empty();
-      }
-    } else {
-      g.selectedCell.imagePath = "bomb.jpg";
+    if (g.selected) {
+        if (g.selectedCell.tag < 100) {
+            g.selectedCell.imagePath = g.selectedCell.tag + "p.png";
+            if (g.selectedCell.tag == 0) {
+                return empty();
+            }
+        } else {
+            g.selectedCell.imagePath = "bomb.jpg";
+        }
     }
-  }
 }
